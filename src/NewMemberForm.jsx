ff9ece82f93fc67cc79de827404d8b1ebf5e2ad8
isabelxklee <hello@isabelklee.co>
2020-10-23 14:33:59 -0400

    updated the form

--- src/NewMemberForm.jsx
+++ src/NewMemberForm.jsx
@@ -2,63 +2,41 @@ import React, { Component } from 'react';
 
 class NewMemberForm extends Component {
   state = {
-    name: "",
-    role: "",
-    location: "",
-    picture: ""
+    // write attributes for each input field
   }
 
   handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
+    // update the state with the user's input
   }
   
   handleSubmit = (event) => {
-    event.preventDefault()
-
-    fetch("http://localhost:3000/team-members", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        name: this.state.name,
-        role: this.state.role,
-        location: this.state.location,
-        picture: this.state.picture,
-        stars: 0
-      })
-    })
-    .then(r => r.json())
-    .then((newMember) => {
-      this.props.addNewMember(newMember)
-    })
+    // insert event.preventDefault() to stop the page from refreshing upon submit
+    // write a fetch POST request that creates a new team member and sends that data to the backend
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form>
         <h2>Add a new member to the SuperHi team</h2>
 
         <label>Name</label>
         <br />
-        <input type="text" name="name" value={this.state.value} onChange={this.handleChange} />
+        <input type="text" name="name" />
         <br />
 
         <label>Role</label>
         <br />
-        <input type="text" name="role" value={this.state.value} onChange={this.handleChange} />
+        <input type="text" name="role" />
         <br />
 
         <label>Location</label>
         <br />
-        <input type="text" name="location" value={this.state.value} onChange={this.handleChange} />
+        <input type="text" name="location" />
         <br />
 
         <label>Picture URL</label>
         <br />
-        <input type="text" name="picture" value={this.state.value} onChange={this.handleChange} />
+        <input type="text" name="picture" />
         <br />
 
         <input type="submit" value="Add new member" />
