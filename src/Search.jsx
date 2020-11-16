aaf0c5a8a65d69f09fccdbf66d3b08944e331bb2
isabelxklee <hello@isabelklee.co>
2020-11-16 17:20:49 -0500

    updated search component

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,5 +1,23 @@
 import React from 'react'
 
+// const handleChange = ({props, event}) => {
+//   props.handleSearchTerm(event.target.value)
+// }
+
+// const Search = ({searchTerm, handleSearchTerm}) => (
+//   <div id="search">
+//     <label>Filter by location</label>
+//     <br />
+//     <input
+//       type="text"
+//       name="search"
+//       placeholder="Enter a location"
+//       value={searchTerm}
+//       onChange={handleChange}
+//     />
+//   </div>
+// )
+
 const Search = (props) => {
   let handleChange = (event) => {
     props.handleSearchTerm(event.target.value)
